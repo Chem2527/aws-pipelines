d6b046adb42eed6ac7089f664be7a91876c15ab7
K.Saikrishna <108404892+Chem2527@users.noreply.github.com>
2025-04-04 20:29:42 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,9 @@
-sudo apt update
-sudo apt install -y python3 python3-pip
-pip3 install -r /home/ubuntu/app/requirements.txt
+#!/bin/bash
+# Use yum instead of apt
+echo "Installing system dependencies"
+sudo yum update -y
+sudo yum install -y python3 python3-pip  # Adjust to install the necessary packages
+
+# If you need to install Python dependencies from requirements.txt
+echo "Installing Python dependencies"
+pip3 install -r requirements.txt
